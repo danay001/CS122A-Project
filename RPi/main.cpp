d1755ac29e7191f91907c084749a2f3b248b1032
David Anaya <danay001@ucr.edu>
2019-11-15 16:09:47 -0800

    Get ultrasonic sensor working

---
+++ RPi/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <wiringPi.h>
+#include <chrono>
+
+using namespace std::chrono;
+
+#define TRIG 4
+#define ECHO 5
+
+enum DetectSM{Wait, Bump, Wall} detect_state;
+
+unsigned long time_us(){
+
+       	microseconds us = duration_cast< microseconds >(system_clock::now().time_since_epoch());
+	return us.count();
+}
+
+double dist_from_wall(){
+	double dist = 0.0;
+	unsigned long pulse_start = 0;
+	unsigned long pulse_end = 0;
+	unsigned long pulse_duration = 0;
+
+	digitalWrite(TRIG, 1);
+	delayMicroseconds(10);
+	digitalWrite(TRIG, 0);
+		
+	while(digitalRead(ECHO) == 0){
+		pulse_start = time_us();  //Get timestap right before rising edge of ECHO
+	}
+
+	while(digitalRead(ECHO)){
+		pulse_end = time_us();	//Get timestamp right before falling edge of ECHO
+	}
+
+	pulse_duration = pulse_end - pulse_start;
+	dist = static_cast<double>(pulse_duration) / 58;  //Get distance in cm
+
+	return dist;
+}
+
+void Obj_Detection(){
+	static double curDist = 0.0;
+	switch(detect_state){
+		case Wait:
+			curDist = dist_from_wall();
+                        if(digitalRead(6)){
+				detect_state = Bump;
+			}
+			else if(curDist <= 5.0){
+				detect_state = Wall;
+			}
+			else{
+				detect_state = Wait;
+			}
+			break;
+
+		case Bump:
+			if(digitalRead(6)){
+				detect_state = Bump;
+
+			}
+			else{
+				detect_state = Wait;
+			}
+
+			break;
+		case Wall:
+			detect_state = Wait;
+			break;
+
+
+	}
+
+	switch(detect_state){
+		case Wait:
+			digitalWrite(0, 0);
+			break;
+
+		case Bump:
+			digitalWrite(0, 1);
+			break;
+
+		case Wall:
+			digitalWrite(0, 1);
+			break;
+
+	}
+}
+
+int main(){
+	wiringPiSetup();
+	pinMode(TRIG, OUTPUT);
+	pinMode(ECHO, INPUT);
+
+	digitalWrite(TRIG, 0);
+	delay(2000);
+
+	//Initialize state variables
+	detect_state = Wait;
+
+	while(1){
+		Obj_Detection();
+
+		delay(50);
+
+	}
+
+	return 0;
+}
+
